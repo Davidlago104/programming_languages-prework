8b04ffef32e2eaf36cc44b699146ce8acf59dde1
Davidlago104 <davidlago104@gmail.com>
2018-07-07 15:25:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+new_hash = {}
+languages.each do |k, v|
+  v.each do |language, type|
+    new_hash[language] = type
+    if language ==:ruby 
+      new_hash[language].merge!({:style => [:oo]})
+      elsif language == :javascript 
+      new_hash[language].merge!({:style => [:oo, 
+    end
+  end
 end
+new_hash
+ end
